6c8e341933847ee5db9c67b3676467aed494ae4f
operativoscuenta <operativoscuenta@gmail.com>
2013-09-10 05:06:30 -0500

    Se crea proyecto local

---
+++ src/Imprimir.java
@@ -0,0 +1,14 @@
+public class Imprimir {
+    public boolean devuelve_mensajeDos(boolean dato) {
+        if (dato != true) {
+            System.out
+                    .println("Aja 2 MNS programa de prueba ImprimeDos para repo en Git  falso");
+            dato = false;
+        } else {
+            System.out
+                    .println("Aja 2 MNS programaa de prueba ImprimeDos para repo en Git true");
+            dato = true;
+        }
+        return dato;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] parametro) {
+        System.out
+                .println("=============Aja Cambio de Julian Repo Programa de MODIFICADO DOS prueba para GIT CON hub============");
+        Imprimir imprimir = new Imprimir();
+        imprimir.devuelve_mensajeDos(true);
+        // imprimeDos.devuelve_mensajeDos(false);
+    }
+}
